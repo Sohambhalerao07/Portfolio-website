d482e68c7900267eea857325a46acea70a02e85b
Soham bhalerao <bhaleraosoham07@gmail.com>
2024-10-30 18:50:40 +0530

    new update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Portfolio-website/"
+  base:"/Portfolio-website/",
+  build: {
+    rollupOptions: {
+      external: ['react-simple-typewriter'],
+    },
+  }
 })
